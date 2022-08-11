d0c5cefa754dc1536a9df60959657e73a4b0bf07
RacketyWater7 <haseebudeen@outlook.com>
2022-08-11 10:43:59 +0500

    added an exception

--- js/content-script.js
+++ js/content-script.js
@@ -128,18 +128,18 @@ const populateTicketDetails = (data) => {
     }
     if (yes) {
       yes.checked = true;
+
+      let NextBtn = document.getElementById(
+        "ContentPlaceHolder1_shiptsubmit_Button"
+      );
+      if (NextBtn) {
+        setTimeout(() => {
+          NextBtn.click();
+          NextBtn.dispatchEvent(new Event("click"));
+        }, 1600);
+      }
     }
   }, 1400);
-
-  let NextBtn = document.getElementById(
-    "ContentPlaceHolder1_shiptsubmit_Button"
-  );
-  if (NextBtn) {
-    setTimeout(() => {
-      NextBtn.click();
-      NextBtn.dispatchEvent(new Event("click"));
-    }, 1600);
-  }
 };
 /**
  * This function populates the passenger details
